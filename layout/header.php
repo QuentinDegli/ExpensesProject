ded290a32167fd26ea5300f29af623702931f272
NICOLAS Quentin <quentinnicolasdegli@gmail.com>
2023-08-28 16:36:31 +0200

    first commit

---
+++ layout/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+    <title>Gestionnaire de dépenses</title>
+</head>
+<body>
+
+<nav>
+    <ul>
+        <li>
+            <a href="index.php">Page d'acceuil</a>
+        </li>
+        <li>
+            <a href="add-expenses.php">Ajout d'une dépense</a>
+        </li>
+        <li>
+            <a href="recap-expenses.php">Ma synthèse</a>
+        </li>
+    </ul>
+</nav>
